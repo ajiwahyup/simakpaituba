fdb3dcf158b1291138319b8ada3a97beaee3568c
Wahyu Aji P <aji.wahyu77@gmail.com>
2021-11-08 22:16:30 +0700

    First

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    // Animated on Scroll
+    function aos_init() {
+        AOS.init({
+            duration: 1000,
+            once: true,
+            mirror: true
+        })
+    }
+    aos_init()
+})
